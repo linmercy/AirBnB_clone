ba60270f3251adc6e45cde92c9150b5c300f3b68
mercylin <linmercymuthoni@gmail.com>
2024-03-10 23:51:08 +0000

    save and reload

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for BaseModel class."""
 
+from models import storage
 import uuid
 from datetime import datetime
 
@@ -8,17 +9,23 @@ from datetime import datetime
 class BaseModel:
     """Defines the common attributes/methods for other classes."""
 
+
     def __init__(self, *args, **kwargs):
+        """Initialize BaseModel instance."""
         if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ['created_at', 'updated_at']:
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance."""
@@ -37,6 +44,7 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute updated_at."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return dictionary representation of BaseModel instance."""
