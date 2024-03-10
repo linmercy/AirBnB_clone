4f51dc848d41c7bca6e863bf79bd52db9d1cdc0e
mercylin <linmercymuthoni@gmail.com>
2024-03-10 23:29:33 +0000

    dictionaries

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,24 @@ class BaseModel:
     """Defines the common attributes/methods for other classes."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize BaseModel instance."""
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
 
     def __str__(self):
         """Return string representation of BaseModel instance."""
